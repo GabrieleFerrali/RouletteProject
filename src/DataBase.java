72bf8283c298ff21d9b64e232f9c64ead5ea2eb8
GabrieleFerrali <47115399+GabrieleFerrali@users.noreply.github.com>
2019-09-21 12:32:10 +0200

    Add Roulette Files to src

---
+++ src/DataBase.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bbbaden.roulette;
+
+/**
+ *
+ * @author gabri
+ */
+public class DataBase {
+    private final int konto;
+
+    public DataBase(int konto) {
+        this.konto = konto;
+    }
+    
+    public int getKonto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setKonto(int konto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
